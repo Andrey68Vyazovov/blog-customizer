9ea0b53be2b1c47f0acdd1d0a3d2b44acedfac99
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2024-03-04 21:07:08 +0300

    commit for review

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useRef, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,19 +13,60 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const dataStorage = JSON.parse(String(localStorage.getItem('state')));
+	const optionsRef = useRef({
+		fontFamilyOption: dataStorage
+			? dataStorage.fontFamilyOption
+			: defaultArticleState.fontFamilyOption,
+		fontColor: dataStorage
+			? dataStorage.fontColor
+			: defaultArticleState.fontColor,
+		backgroundColor: dataStorage
+			? dataStorage.backgroundColor
+			: defaultArticleState.backgroundColor,
+		contentWidth: dataStorage
+			? dataStorage.contentWidth
+			: defaultArticleState.contentWidth,
+		fontSizeOption: dataStorage
+			? dataStorage.fontSizeOption
+			: defaultArticleState.fontSizeOption,
+	});
+
+	const [option, setOption] = useState(optionsRef.current);
+
+	function handleChangeOptions({
+		fontFamilyOption,
+		fontColor,
+		backgroundColor,
+		contentWidth,
+		fontSizeOption,
+	}: typeof defaultArticleState) {
+		optionsRef.current = {
+			fontFamilyOption: fontFamilyOption,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+			fontSizeOption: fontSizeOption,
+		};
+		setOption(optionsRef.current);
+	}
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': option.fontFamilyOption.value,
+					'--font-size': option.fontSizeOption.value,
+					'--font-color': option.fontColor.value,
+					'--container-width': option.contentWidth.value,
+					'--bg-color': option.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				props={optionsRef.current}
+				onChange={handleChangeOptions}
+			/>
 			<Article />
 		</div>
 	);
